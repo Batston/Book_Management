74b58a003726a19edb44d1d52d13ab3d02214afc
Phuong <vuminhphuong9029@gmail.com>
2024-11-07 12:32:45 +0700

    add vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,60 +1,23 @@
 <template>
   <v-app>
-    <v-card>
-      <v-layout>
-        <v-app-bar color="primary" prominent>
-          <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-
-          <v-toolbar-title>My files</v-toolbar-title>
-
-          <v-spacer></v-spacer>
-
-          <template v-if="$vuetify.display.mdAndUp">
-            <v-btn icon="mdi-magnify" variant="text"></v-btn>
-
-            <v-btn icon="mdi-filter" variant="text"></v-btn>
-          </template>
-
-          <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
-        </v-app-bar>
-
-        <v-navigation-drawer v-model="drawer" :location="$vuetify.display.mobile ? 'bottom' : undefined" temporary>
-          <v-list>
-            <v-list>
-              <v-list-item to="/login" router-link>
-                <v-list-item-content>
-                  <v-list-item-title>Login</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-              <v-list-item to="/home" router-link>
-                <v-list-item-content>
-                  <v-list-item-title>Home</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-list>
-        </v-navigation-drawer>
-
-        <v-main style="height: 500px;">
-        </v-main>
-      </v-layout>
-    </v-card>
     <v-main>
-      <router-view></router-view>
+      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
 
   components: {
+    HelloWorld,
   },
 
   data: () => ({
-    drawer: false,
+    //
   }),
 }
 </script>
