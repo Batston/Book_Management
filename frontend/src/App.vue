7b7b0564303bbe030e788c635409c5aaf46a2e12
Phuong <vuminhphuong9029@gmail.com>
2024-11-07 12:38:58 +0700

    navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <v-app>
+    <v-app-bar color="primary" prominent>
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Book Store Manager</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <template v-if="$vuetify.display.mdAndUp">
+        <v-btn icon="mdi-magnify" variant="text"></v-btn>
+        <v-btn icon="mdi-filter" variant="text"></v-btn>
+      </template>
+      <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" :location="$vuetify.display.mobile ? 'bottom' : undefined" temporary>
+      <v-list>
+        <v-list>
+          <v-list-item to="/login" router-link>
+            <v-list-item-content>
+              <v-list-item-title>Login</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/home" router-link>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
-    //
+    drawer: false
   }),
 }
 </script>
