1224b54916bf90a0ca5f090b04bcd29afe9e5d5a
Phuong <vuminhphuong9029@gmail.com>
2024-11-07 12:24:03 +0700

    add frontend

--- backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e24bdcc9a1ac62292162cd1135eb084821416b1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
